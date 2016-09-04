d7859940c311f60c36665c276689323de4ff9c1a
spsinghdocument <spsinghdocument@gmail.com>
2016-09-03 20:54:35 -0700

    web api

---
+++ web api token/AngularJsApplication/WebAPI/Controller/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controller
+{
+    [Authorize]
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult TestAuthorize()
+        {
+            return Ok("saurabh pratap singh");
+        }
+    }
+}
---
+++ web api token/web api/WebApplication42/Controllers/spController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication42.Controllers
+{
+    [Authorize]
+    public class spController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult TestAuthorize()
+        {
+            return Ok("saurabh pratap singh");
+        }
+    }
+}
